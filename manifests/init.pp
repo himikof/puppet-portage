552d91cb04f88c3c55d5e8cb958ed4a539988865
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2012-08-07 22:04:52 +0400

    Disable timeout on emerge

--- manifests/init.pp
+++ manifests/init.pp
@@ -120,6 +120,7 @@ class portage (
   exec { 'portage_changed_config':
     command     => "/usr/bin/emerge --changed-use --deep @world",
     refreshonly => "true",
+    timeout     => 0,                   # Disable timeout completely
   }
 
   # Portage configuration
